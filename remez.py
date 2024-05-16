3fdbeb06838d22a4577c12af9d949a94e78d7af1
Jeremy Kun <j2kun@users.noreply.github.com>
2024-05-16 16:16:17 -0700

    continue with remez

--- remez.py
+++ remez.py
@@ -1,5 +1,6 @@
 """An implementation of the Remez algorithm."""
 
+import functools
 import math
 import numpy as np
 import polynomial
@@ -80,6 +81,98 @@ def build_linear_system(xs, fxs):
   # (-1)**j; does it matter if I flip all these signs?
   A[:, n + 1] = (-1) ** np.arange(n + 2)
   return A, np.array(fxs)
+
+
+def secant_method(f, a, b, iters=100, precision=1e-15):
+  """Apply the secant method to find the roots of f within [a,b]."""
+  if f(a) * f(b) > 0:
+    raise ValueError("f(a) and f(b) must have different signs.")
+
+  xs = (a, b)
+  for _ in range(iters):
+    next_x = xs[-1] - f(xs[-1]) * (xs[-1] - xs[-2]) / (f(xs[-1]) - f(xs[-2]))
+    xs = (xs[-1], next_x)
+    f_next_x = f(next_x)
+    if abs(f_next_x) < precision or abs(xs[0] - xs[1]) < precision:
+      return next_x
+
+  print(f"Warning: hit {iters} iters without reaching precision {precision}.")
+  return next_x
+
+
+def compute_min(f, a, b):
+  """Find the min of f within [a,b].
+
+  Consider Brent's method, porting the scipy implementation at
+  https://github.com/scipy/scipy/blob/7dcd8c59933524986923cde8e9126f5fc2e6b30b/scipy/optimize/_optimize.py#L2430
+  """
+  pass
+
+
+def compute_max(f, a, b):
+  """Find the max of f within [a,b]."""
+  return compute_min(lambda x: -f(x), a, b)
+
+
+def exchange(xs, errs, levelled_error, cheb_coeffs, f):
+  """Improve the reference point set for one iteration of the Remez algorithm.
+
+  Args:
+    xs: the current set of reference points x_i.
+    errs: the values of p(x_i) - f(x_i), where p is the current approximation
+      and f is the desired function to approximate.
+    levelled_error: the value h for which p(x_i) = f(x_i) +/- h (n.b., with
+      alternating signs as i increases) for all i.
+    cheb_coeffs: the Chebyshev coefficients of the current candidate polynomial.
+    f: the function to be approximated, as a callable.
+
+  Returns:
+    - The new reference point set.
+    - The differences between the error function values at the old and new
+      reference points.
+  """
+  # Step 1: find the roots of the error function.
+  # Since f is arbitrary, the error function is also arbitrary.
+  # In some cases we might be able to utilize a known derivative of f, and
+  # compute a known derivative of p, and use that to apply Newton's method.
+  # For this implementation we are arbitrary, and instead apply the secant
+  # method.
+  roots = []
+
+  @functools.lru_cache(maxsize=100)
+  def error_fn(x):
+    return polynomial.cheb_eval(cheb_coeffs, x) - f(x)
+
+  for i in range(len(xs) - 1):
+    # Because the sign of the error oscillates, there is a root between
+    # each successive pair of x_i.
+    roots.append(secant_method(error_fn, xs[i], xs[i + 1]))
+
+  # Now that we have the roots, there is a guaranteed extremum between
+  # each pair of roots, including the endpoints of the interval [-1, 1].
+  # Since this set of extrema_bounds has size n+3, we end with n+2 new
+  # control points.
+  extrema = []
+  extrema_bounds = [-1] + roots + [1]
+  for i in range(len(extrema_bounds) - 1):
+    corresponding_error_value = errs[i]
+    looking_for_min = corresponding_error_value < 0
+    a, b = extrema_bounds[i], extrema_bounds[i + 1]
+    extrema.append(
+        compute_min(error_fn, a, b)
+        if looking_for_min
+        else compute_max(error_fn, a, b)
+    )
+
+  for ext in extrema:
+    if error_fn(ext) < h:
+      print(
+          "Warning: found extrema that are worse than starting point."
+          " Shenanigans are afoot!"
+      )
+
+  diffs = [error_fn(ext) - err for (ext, err) in zip(extrema, errs)]
+  return (extrema, diffs)
 
 
 def estimate_error(cheb_coeffs, f):
