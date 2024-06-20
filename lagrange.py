cbc37b781cd3fe70fce0c728223d5ea7c18b53c6
Jeremy Kun <j2kun@users.noreply.github.com>
2024-06-20 16:58:14 -0700

    support barycentric formula for chebyshev nodes

--- lagrange.py
+++ lagrange.py
@@ -1,6 +1,7 @@
 """Routines for Lagrange interpolation."""
 
-def compute_barycentric_weights(xs):
+
+def compute_barycentric_weights(xs, a=-1, b=1):
     """Compute barycentric weights for Lagrange interpolation."""
     n = len(xs)
     weights = [1] * n
@@ -8,21 +9,50 @@ def compute_barycentric_weights(xs):
         for k in range(n):
             if j == k:
                 continue
-            weights[j] *= (xs[j] - xs[k])
+            weights[j] *= xs[j] - xs[k]
 
+    C = (b - a) / 4
+    C_inv = 1 / C
     for j in range(n):
-        weights[j] = 1.0 / weights[j]
+        weights[j] = 1.0 / (C_inv * weights[j])
 
     return weights
 
 
-def barycentric_lagrange(xs, ys):
+def compute_barycentric_weights_chebyshev(xs, a=-1, b=1):
+    """Compute barycentric weights for Lagrange interpolation
+    on Chebyshev nodes of the second kind in [-1, 1].
+
+    Computes the formula w_j = (-1)^j d_j for j = 0, ..., n=len(xs),
+    where d_0 = d_n = 0.5, d_j = 1 otherwise.
+    """
+    n = len(xs)
+    weights = [1] * n
+    weights[0] = 0.5
+    weights[-1] = 0.5
+    for j in range(1, n, 2):
+        weights[j] = -weights[j]
+
+    # This is the official formula for the weight change, but it's not
+    # necessary because it occurs in the numerator and denominator of the
+    # interpolation formula.
+    #
+    # if a != -1 or b != 1:
+    #     scale = 2**n / (b - a)**n
+    #     for j in range(n):
+    #         weights[j] *= scale
+
+    return weights
+
+
+def barycentric_lagrange(xs, ys, weights_computer=compute_barycentric_weights):
     """Compute the Lagrange interpolating polynomial of the points (x, y)."""
-    weights = compute_barycentric_weights(xs)
+    weights = weights_computer(xs)
+
     def f(x):
         numerator = 0
         denominator = 0
-        for (w, xi, yi) in zip(weights, xs, ys):
+        for w, xi, yi in zip(weights, xs, ys):
             if x == xi:
                 return yi
             common_factor = w / (x - xi)
@@ -31,4 +61,3 @@ def barycentric_lagrange(xs, ys):
         return numerator / denominator
 
     return f
-
--- lagrange_test.py
+++ lagrange_test.py
@@ -11,9 +11,47 @@ def test_lagrange_interpolation():
     ys = [f(x) for x in xs]
 
     actual = lagrange.barycentric_lagrange(xs, ys)
-    for (x, y) in zip(xs, ys):
+    for x, y in zip(xs, ys):
         assert abs(f(x) - actual(x)) < 1e-08
 
     for i in range(-50, 50):
         assert abs(f(i) - actual(i)) < 1e-08
 
+
+def test_lagrange_interpolation_chebyshev():
+    # f(x) = x^2 + 3x + 2
+    def f(x):
+        return x * x + 3 * x + 2
+
+    xs = remez.initial_reference(3)
+    ys = [f(x) for x in xs]
+
+    actual = lagrange.barycentric_lagrange(
+        xs,
+        ys,
+        weights_computer=lagrange.compute_barycentric_weights_chebyshev,
+    )
+    for x, y in zip(xs, ys):
+        assert abs(f(x) - actual(x)) < 1e-08
+
+    for i in range(-50, 50):
+        assert abs(f(i) - actual(i)) < 1e-08
+
+
+def test_lagrange_interpolation_for_non_polynomial():
+    def f(x):
+        return abs(x) + x / 2 - x * x
+
+    xs = remez.initial_reference(100000)
+    ys = [f(x) for x in xs]
+
+    actual = lagrange.barycentric_lagrange(
+        xs,
+        ys,
+        weights_computer=lagrange.compute_barycentric_weights_chebyshev,
+    )
+
+    for i in range(-100, 100):
+        x = i / 100
+        # can't get fast convergence because |x| is not analytic
+        assert abs(f(x) - actual(x)) < 1e-04
